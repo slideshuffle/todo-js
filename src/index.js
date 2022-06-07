cca57335e51a69cb06a09ecba430274040d0bf5e
slideshuffle <52997518+slideshuffle@users.noreply.github.com>
2022-06-07 11:41:56 +0900

    jsを更新

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  //div生成
+  const div = document.createElement("div");
+  div.innerText = inputText;
+
+  //liタグの子要素に各要素を設定
+  li.appendChild(div);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document
