175066b6901561e0b4b358c49aade37f03b69e66
slideshuffle <52997518+slideshuffle@users.noreply.github.com>
2022-06-07 13:03:17 +0900

    jsを更新

--- src/index.js
+++ src/index.js
@@ -9,10 +9,33 @@ const onClickAdd = () => {
 
   //div生成
   const div = document.createElement("div");
-  div.innerText = inputText;
+
+  //p生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(li)を未完了リストから削除
+    // const deleteTarget = deleteButton.parentNode;
+    const deleteTarget = deleteButton.closest(".list-row");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   //liタグの子要素に各要素を設定
   li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
